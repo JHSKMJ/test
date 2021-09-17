0d350a4a5b407a5d0dedfebf37de5f56e3210e69
JHSKMJ <57669831+JHSKMJ@users.noreply.github.com>
2021-09-17 15:36:08 +0900

    0917

---
+++ app/src/main/java/com/example/a0915test/navigation/UserFragment.kt
@@ -0,0 +1,16 @@
+package com.example.a0915test.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.View
+import com.example.a0915test.R
+import java.util.zip.Inflater
+
+class UserFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
+        return view
+    }
+}
